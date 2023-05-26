2544a1f8aa33698cf20ed0159ec6db40c660df54
ina-stoyanova <ina@gruntwork.io>
2023-05-26 13:09:28 +0100

    hello world app code

---
+++ go.mod
@@ -0,0 +1,5 @@
+module aws-lambda-api-gateway
+
+go 1.19
+
+require github.com/aws/aws-lambda-go v1.41.0 // indirect
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(handleRequest)
+}
+
+func handleRequest(events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	response := events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello World",
+	}
+
+	return response, nil
+}
